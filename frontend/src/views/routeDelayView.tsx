5d99ec366ad71f6a4d212a0dba69fd81f2902436
Daniel Ruijs <daniel.ruijs@gmail.com>
2026-04-26 20:20:04 +0200

    feat: split RouteDelayView into separate screen-level views

--- frontend/src/views/routeDelayView.tsx
+++ frontend/src/views/routeDelayView.tsx
@@ -2,20 +2,11 @@ import { Box, Paper, Typography } from "@mui/material";
 import { RouteDelayControls } from "../components/RouteDelayControls";
 import { DatePreset, EventType } from "../types/departureDelay";
 import { TransportationMode } from "../types/sl";
-import {
-    PageSizeOption,
-    RouteDelayListItem,
-    RouteDelaySection,
-    RouteDelayTrendPoint,
-} from "../types/routeDelays";
-import { DelaySummary } from "../types/historicalDelay";
-import { RouteDelayRoutesView } from "./routeDelayRoutesView";
-import { RouteDelayLeaderboardView } from "./routeDelayLeaderboardView";
-import { RouteDelayRouteFallbackView } from "./routeDelayRouteFallbackView";
-import { RouteDetailsPage } from "../components/RouteDetailsPage";
+import { RouteDelaySection } from "../types/routeDelays";
 import { RouteDelaySectionToggleView } from "./routeDelaySectionToggleView";
 import { RouteDelayInfoView } from "./RouteDelayInfoView";
 import { TranslationStrings } from "../utils/translations";
+import { ReactNode } from "react";
 
 type RouteDelayViewProps = {
     selectedSection: RouteDelaySection;
@@ -26,15 +17,8 @@ type RouteDelayViewProps = {
     selectedEventType: EventType;
     selectedTransportationMode: TransportationMode;
     searchQuery: string;
-    pagedRouteItems: RouteDelayListItem[];
-    currentPage: number;
-    totalPages: number;
-    routesPerPage: PageSizeOption;
-    selectedRouteKey: string | null;
-    selectedRouteSummary: DelaySummary | null;
-    leaderboardItems: RouteDelayListItem[];
-    trendPoints: RouteDelayTrendPoint[];
-    isTrendLoading: boolean;
+    isRouteDetailsOpen: boolean;
+    content: ReactNode;
     transportationModeOptions: TransportationMode[];
     availableDates: string[];
     onDatePresetChange: (preset: DatePreset) => void;
@@ -43,19 +27,10 @@ type RouteDelayViewProps = {
     onTransportationModeChange: (filter: TransportationMode) => void;
     onSearchQueryChange: (query: string) => void;
     onSelectedSectionChange: (section: RouteDelaySection) => void;
-    onSelectRoute: (routeKey: string | null) => void;
-    onBackToRoutes: () => void;
-    onPageChange: (nextPage: number) => void;
-    onRoutesPerPageChange: (nextPageSize: PageSizeOption) => void;
     tRouteDelay: TranslationStrings["routeDelay"];
     tSectionToggle: TranslationStrings["routeDelaySectionToggle"];
-    tRoutes: TranslationStrings["routeDelayRoutes"];
-    tLeaderboard: TranslationStrings["routeDelayLeaderboard"];
-    tRouteFallback: TranslationStrings["routeDelayRouteFallback"];
     tControls: TranslationStrings["routeDelayControls"];
-    tDetailsPage: TranslationStrings["routeDetailsPage"];
     tDatePicker: TranslationStrings["availableDatesPicker"];
-    tStats: TranslationStrings["departureDelayStats"];
     tTransportModes: TranslationStrings["transportModes"];
 };
 
@@ -68,15 +43,8 @@ export function RouteDelayView({
     selectedEventType,
     selectedTransportationMode,
     searchQuery,
-    pagedRouteItems,
-    currentPage,
-    totalPages,
-    routesPerPage,
-    selectedRouteKey,
-    selectedRouteSummary,
-    leaderboardItems,
-    trendPoints,
-    isTrendLoading,
+    isRouteDetailsOpen,
+    content,
     transportationModeOptions,
     availableDates,
     onDatePresetChange,
@@ -85,23 +53,12 @@ export function RouteDelayView({
     onTransportationModeChange,
     onSearchQueryChange,
     onSelectedSectionChange,
-    onSelectRoute,
-    onBackToRoutes,
-    onPageChange,
-    onRoutesPerPageChange,
     tRouteDelay,
     tSectionToggle,
-    tRoutes,
-    tLeaderboard,
-    tRouteFallback,
     tControls,
-    tDetailsPage,
     tDatePicker,
-    tStats,
     tTransportModes,
 }: RouteDelayViewProps) {
-    const isRouteDetailsOpen = selectedRouteKey !== null;
-
     return (
         <Box
             sx={{
@@ -170,46 +127,7 @@ export function RouteDelayView({
                         />
                     </div>
 
-                    <div>
-                        {selectedRouteKey === null ? (
-                            <div className="flex flex-col gap-4 pt-4">
-                                {selectedSection === "routes" ? (
-                                    <RouteDelayRoutesView
-                                        pagedRouteItems={pagedRouteItems}
-                                        currentPage={currentPage}
-                                        totalPages={totalPages}
-                                        routesPerPage={routesPerPage}
-                                        onSelectRoute={onSelectRoute}
-                                        onPageChange={onPageChange}
-                                        onRoutesPerPageChange={onRoutesPerPageChange}
-                                        t={tRoutes}
-                                    />
-                                ) : null}
-
-                                {selectedSection === "leaderboard" ? (
-                                    <RouteDelayLeaderboardView
-                                        leaderboardItems={leaderboardItems}
-                                        t={tLeaderboard}
-                                    />
-                                ) : null}
-                            </div>
-                        ) : selectedRouteSummary ? (
-                            <RouteDetailsPage
-                                routeSummary={selectedRouteSummary}
-                                selectedEventType={selectedEventType}
-                                trendPoints={trendPoints}
-                                isTrendLoading={isTrendLoading}
-                                onBackToRoutes={onBackToRoutes}
-                                t={tDetailsPage}
-                                tStats={tStats}
-                            />
-                        ) : (
-                            <RouteDelayRouteFallbackView
-                                onBackToRoutes={onBackToRoutes}
-                                t={tRouteFallback}
-                            />
-                        )}
-                    </div>
+                    <div>{content}</div>
                 </div>
             </Paper>
         </Box>
